5c2b8fea8885e1155af9d0018bd21ef1eb8d28c9
lamborghinigamer1 <61702082+lamborghinigamer1@users.noreply.github.com>
2023-10-05 12:25:56 +0200

    Made build.sh dynamic

--- build.sh
+++ build.sh
@@ -9,6 +9,6 @@ fi
 
 echo "Compiling blackjack"
 
-g++ -std=c++11 src/Card.cpp src/Player.cpp src/Deck.cpp src/main.cpp -o target/blackjack
+g++ -std=c++11 src/*.cpp -o target/blackjack
 
 echo "Compiling done in target/."
